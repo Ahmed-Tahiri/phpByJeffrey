16a4de96596e7cc42874315a36c4480b7c1021ef
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-12-31 10:09:43 +0500

    Partials  Completed

---
+++ 10.partials/views/partials/navbar.php
@@ -0,0 +1,13 @@
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+            <span class="fs-4 text-light">About Page</span>
+        </a>
+
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a href="/" class="nav-link " aria-current="page">Home</a></li>
+            <li class="nav-item"><a href="./about.php" class="nav-link active">About</a></li>
+            <li class="nav-item"><a href="./contact.php" class="nav-link">Contact</a></li>
+        </ul>
+    </header>
+</div>
