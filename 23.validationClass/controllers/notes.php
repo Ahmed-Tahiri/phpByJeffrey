8dd8dc29374969fede49e973d22cfc582690e687
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2025-01-06 15:03:11 +0500

    Validation Class Created

---
+++ 23.validationClass/controllers/notes.php
@@ -0,0 +1,7 @@
+<?php
+$heading = "Notes";
+$config = require('config.php');
+$db = new Database($config['database'], 'root', 'Ahmed1682&9');
+$query = "SELECT * FROM `notes`;";
+$notes = $db->query($query)->get();
+require "views/notes.view.php";
